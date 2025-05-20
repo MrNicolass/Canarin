8fe614f18c74cf24719ad48e91fbefbad0a03a1c
Nicolas Gustavo Conte <80847876+MrNicolass@users.noreply.github.com>
2025-05-19 21:54:40 -0300

    First commit configuration

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,12 +12,12 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/helpetslohoexemplo.svg')}
           style={styles.reactLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Canarin!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
   },
   reactLogo: {
     height: 178,
-    width: 290,
+    width: 230,
     bottom: 0,
     left: 0,
     position: 'absolute',
