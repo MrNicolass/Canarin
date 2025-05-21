aab80519b707bfffeb0dd2ee97190cbc886345fd
AoiteFoca <nathancielusinski@gmail.com>
2025-05-20 21:01:18 -0300

    Adilção do perfil ao layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -47,6 +47,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="arrow.up.to.line" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="perfil"
+        options={{
+          title: 'Perfil',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
